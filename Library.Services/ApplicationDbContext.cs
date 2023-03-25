a53f15b59391b9538000d0c7cbe8a23c03de5de8
Евгений Якубович <yakubovich_e@inbox.ru>
2023-03-25 19:29:13 +0300

    rename

---
+++ Library.Services/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Entities;
+
+namespace Library.Services
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public DbSet<Gender> Genders { get; set; }
+
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
---
+++ Library/Controllers/ConfigController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Library.Entities;
+using Library.Models;
+using Library.Services;
+using Library.Services.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Library.Controllers
+{
+	[Route("Config")]
+    public class ConfigController : ControllerBase
+    {
+		private readonly ILogger<UsersController> _logger;
+		private readonly IConfiguration _configuration;
+
+		public ConfigController(
+			ILogger<UsersController> logger,
+			IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		[HttpPost]
+		public IActionResult SetLanguage(string culture, string returnUrl)
+		{
+			Response.Cookies.Append(
+				CookieRequestCultureProvider.DefaultCookieName,
+				CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+				new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+			);
+
+			return LocalRedirect(returnUrl);
+		}
+	}
+}
