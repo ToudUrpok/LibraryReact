bd766019a6c76a1880e46677397fbc1de1816e9b
y.yakubovich <y.yakubovich@softteco.com>
2023-03-26 15:10:54 +0300

    Extend AppUser migration

---
+++ Library.Services/Migrations/20230326121018_ExtendAppUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Services.Migrations
+{
+    public partial class ExtendAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentificationNumber",
+                table: "AspNetUsers",
+                maxLength: 14,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MidName",
+                table: "AspNetUsers",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_IdentificationNumber",
+                table: "AspNetUsers",
+                column: "IdentificationNumber",
+                unique: true,
+                filter: "[IdentificationNumber] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_IdentificationNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdentificationNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MidName",
+                table: "AspNetUsers");
+        }
+    }
+}
