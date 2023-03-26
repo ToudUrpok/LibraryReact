210e6b3dd0a474df20645f542d9e8f9474b7461a
Евгений Якубович <yakubovich_e@inbox.ru>
2023-03-26 20:55:56 +0300

    update

--- Library.Entities/Session.cs
+++ Library.Entities/Session.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Library.Entities
 {
     public class Session
     {
-        public DateTime StartDate { get; set; }
+        public SessionRequest Request { get; set; }
 
         public DateTime ExpireDate { get; set; }
-
-        public Copy BookCopy { get; set; }
-
-        public ApplicationUser Reader { get; set; }
 
         public ApplicationUser Librarian { get; set; }
     }
---
+++ Library.Entities/SessionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Entities
+{
+    public class SessionRequest
+    {
+        public bool IsApproved { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public Copy BookCopy { get; set; }
+
+        public ApplicationUser Reader { get; set; }
+    }
+}
